1d6b680402b04e987ac6f32a132d75f78ee90348
moises-magalhaes <63241484+moises-magalhaes@users.noreply.github.com>
2021-04-16 15:28:02 +0200

    Hero Image working

--- src/components/common/Hero.js
+++ src/components/common/Hero.js
@@ -1,11 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 // import Button from "react-bootstrap/Button";
-<<<<<<< Updated upstream
-import { baseUrl } from "../settings/Api"
-=======
-import {baseUrl } from "../settings/Api";
->>>>>>> Stashed changes
+import { baseUrl } from "../settings/Api";
 
 function Hero() {
 
@@ -16,24 +12,13 @@ const [ heroData, setData ] = useState([]);
             }, []);
 
     const loadData = async () => {
-<<<<<<< Updated upstream
-       await fetch(baseUrl)
-=======
        await fetch(baseUrl +"/home/")
->>>>>>> Stashed changes
         .then(response => response.json())
-        .then(json => setData(json.hero_banner))
-
-
-        // .then(json => setData(json.hero_banner.url))
-
+        .then(json => setData(json.hero_banner.url))
      
     }
-<<<<<<< Updated upstream
-
     console.log(heroData);
-=======
->>>>>>> Stashed changes
+
 
     const newUrl = baseUrl + heroData;
 
@@ -41,26 +26,11 @@ const [ heroData, setData ] = useState([]);
     console.log(newUrl)
 
     return (
-        <div>
-             <img src="{heroData}" alt="object" width="500" height="600" />
-                   
+        <div>                
             <Jumbotron>
                 <div>{/*-------example 1------------*/}
-                <img src={newUrl} alt="object" />
-                   
-                   {/*--------example 2----------*/}
-                   
-                   {/* {heroData.map()item =>
-                       <h1 key={heroData.item}>
-                        {heroData.item}
-                        </h1>} */}
+                <img src={newUrl} alt="object" />              
                 </div>
-
-                {/* <h1>Hello, world!</h1>
-                <p>
-                    This is a simple hero unit, a simple jumbotron-style component for calling
-                    extra attention to featured content or information.
-                </p> */}
             
             </Jumbotron> 
         </div>
