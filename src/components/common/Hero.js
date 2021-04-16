2958e82223438721350a936c84d17a7c47490f41
moises-magalhaes <63241484+moises-magalhaes@users.noreply.github.com>
2021-04-16 12:57:50 +0200

    Update Hero.js
    not able to show. Only able to console.log

--- src/components/common/Hero.js
+++ src/components/common/Hero.js
@@ -1,48 +1,58 @@
 import React, { useState, useEffect } from 'react';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 // import Button from "react-bootstrap/Button";
+
+
 function Hero() {
+
 const [ heroData, setData ] = useState([]);
+
+
+
 
     useEffect (() => {
         loadData();
-        // const getData = async () => {
-        //   const response =  await fetch ("http://localhost:1337/");
-        //   const json = await response.json();
-        //   console.log(json);
-        // };
-        // getData();
-    }, []);
+            }, []);
+
     const loadData = async () => {
        await fetch("http://localhost:1337/home")
         .then(response => response.json())
-        .then(data => setData(data))
+        .then(json => setData(json.hero_banner))
 
     }
     console.log(heroData);
-    console.log(Object.keys(heroData));
-    console.log(heroData.hero_banner);
+    console.log(Object.entries(heroData));
+    console.log(Object.values(heroData));
+
 
     return (
         <div>
             
             <Jumbotron>
                 <div>
-                {Object.keys(heroData).map(image => (
+                 { heroData.map((item)=> {
+                      return <div>{item.id}</div>
+                       
+                 } )
+                  
+                } 
+
+
+                {/* {Object.keys(heroData).map(image => (
                     <div key={image.id}>{image.url}</div>
                 ))} 
-                 
+                  */}
                 {/* {heroData.hero_banner.map (photo =>
                     <div key={photo.id}>{photo.url}</div>
                 )} */}
 
                 </div>
 
-                <h1>Hello, world!</h1>
+                {/* <h1>Hello, world!</h1>
                 <p>
                     This is a simple hero unit, a simple jumbotron-style component for calling
                     extra attention to featured content or information.
-                </p>
+                </p> */}
             
             </Jumbotron> 
         </div>
