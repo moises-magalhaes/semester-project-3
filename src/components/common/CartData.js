3c8c9e759e55d09bd3a1510a615b0b1e3857dd58
moises-magalhaes <63241484+moises-magalhaes@users.noreply.github.com>
2021-04-24 18:24:08 +0200

    added remove successfully

--- src/components/common/CartData.js
+++ src/components/common/CartData.js
@@ -1,16 +1,15 @@
 import React, { useEffect, useState } from 'react'
 import { Button } from 'react-bootstrap';
-import { Link, useParams } from "react-router-dom";
-import { baseUrl } from "../settings/Api";
-import LocalStorageProducts from './LocalStorageProducts';
+import { Link } from "react-router-dom";
+
 
 
 const cartFromLocalStorage =  JSON.parse(localStorage.getItem("shoes") || "[]");
 
 
-function CartData(props) {
+function CartData() {
 
-    // const [ imageData, setImage ] = useState([]);
+    
     const [ cart, setCart ] = useState([cartFromLocalStorage]);
 
 //     const RemoveFromCart = (cart) => {
@@ -21,16 +20,14 @@ function CartData(props) {
         const data = localStorage.getItem("shoes");
         if (data){
             setCart(JSON.parse(data));
-            // setImage(JSON.parse(data.formats.thumbnail))
         }
      }, [])
 
-     
 
-    // useEffect (() =>{
-    //     localStorage.setItem("shoes", JSON.stringify(cart));
-    //  }, [cart])
-
+     const RemoveFromCart = (productToRemove) => {
+        setCart(cart.filter(product => product !==productToRemove)
+        )
+     }
 
     console.log(cart)
    return (
@@ -44,24 +41,17 @@ function CartData(props) {
                         <h3>{product.title}</h3>
                         <p>{product.description}</p>
                         <h4>{product.price}</h4>
-{/*                         
-                    <LocalStorageProducts title={product.title}
-                        image={product.image} 
-                        price={product.price} 
-                        key={product.id} />  
-                         */}
                         
                         {/* <Link to={`/products/${product.id}`} key={product.price}><Button>Check Product</Button></Link> */}
-                                            
-                        </div>  
+                        <Button onClick={()=> RemoveFromCart(product)}> Remove from cart</Button>
+
+                        <Link to="/products"><Button> back to shopping</Button></Link>    
+                    </div>  
                         
                     
                         ))} 
                     
-                    <Button> Remove from cart</Button>
-
-                    {/* <Button onClick={()=> RemoveFromCart(productData)}> Remove from cart</Button> */}
-                    <Link to="/products"><Button> back to shopping</Button></Link>
+                 
                     
                     {/* <Link to="/cart" cart={Cart}><Button>Go to cart ({cart.length}) </Button></Link> */}
 
