16412681b8f2f457cd62a36b1fd1fd712635c15a
moises-magalhaes <63241484+moises-magalhaes@users.noreply.github.com>
2021-04-30 09:44:32 +0200

    Admin still not fetching

--- src/components/common/AdminUser.js
+++ src/components/common/AdminUser.js
@@ -2,65 +2,72 @@ import React, { useEffect, useState } from "react";
 import { Button } from "react-bootstrap";
 import { baseUrl } from "../settings/Api";
 import AdminLogin from "./AdminLogin";
+import axios from "axios";
 
 function AdminUser() {
-	// useEffect(() => {
-	// 	url();
-	// }, []);
-
-	// const url = async (email, password) => {
-	// 	const data = JSON.stringify({ indentifier: email, password: password });
-	// 	const options = {
-	// 		method: "POST",
-	// 		body: data,
-	// 		headers: {
-	// 			"content-type": "application/json",
-	// 		},
-	// 	};
-	// 	await fetch(baseUrl + `/auth/local`).then((response) => response.json());
-
-	//     catch(error){
-	//         console.log(error);
-	//     }
-	// };
-
-	// const url = baseUrl + "/auth/local";
-	// const data = JSON.stringify({indentifier: email, password: password});
-
-	// const options = {
-	//     method: "POST",
-	//     body: data,
-	//     headers: {
-	//         "content-type": "application/json",
-	//     },
-	// }
-
-	const adminUserInfo = {
-		email: "admin@admin.com",
-		password: "admin123",
-	};
-
 	const [user, setUser] = useState({ name: "", email: "" });
 	const [error, setError] = useState("");
 
 	const Login = (details) => {
-		console.log(details);
-
-		if (
-			details.email === adminUserInfo.email &&
-			details.password === adminUserInfo.password
-		) {
-			console.log("Logged in");
-
-			setUser({
-				name: details.name,
-				email: details.email,
+		axios
+			.post(baseUrl + "/auth/local", {
+				identifier: "admin@admin.com",
+				password: "Pass1234",
+                
+			})
+			.then((response) => {
+				//Store user token in cookie or localstorage, then use it in all next requests.
+				console.log("Your user token", response.data.jwt);
+				console.log("Logged in");
+			})
+			.catch((error) => {
+				// Something wrong with auth. Wrong credentials maybe.
+				console.log("An error occurred:", error.response);
+				setError("Details do not match");
 			});
-		} else {
-			console.log("details do not match");
-			setError("Details do not match");
-		}
-	};
+
+
+            if (
+                details.email === identifier &&
+                details.password === password
+            ) {
+                console.log("Logged in");
+                    setUser({
+                    name: details.name,
+                    email: details.email,
+                });
+            } else {
+                        console.log("details do not match");
+                        setError("Details do not match");
+                    }
+                }
+                
+    // const adminUserInfo = {
+	// 	email: "admin@admin.com",
+	// 	password: "admin123",
+	// };
+
+	// const [user, setUser] = useState({ name: "", email: "" });
+	// const [error, setError] = useState("");
+
+	// const Login = (details) => {
+	// 	console.log(details);
+
+	// 	if (
+	// 		details.email === adminUserInfo.email &&
+	// 		details.password === adminUserInfo.password
+	// 	) {
+	// 		console.log("Logged in");
+
+	// 		setUser({
+	// 			name: details.name,
+	// 			email: details.email,
+	// 		});
+	// 	} else {
+	// 		console.log("details do not match");
+	// 		setError("Details do not match");
+	// 	}
+	// };
 
 	const Logout = () => {
 		setUser({ name: "", email: "" });
